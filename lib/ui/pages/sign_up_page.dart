2d0938418ce28134d8f8423c3e76d9ed4cb8f13d
helloaltop <helloaltop@gmail.com>
2022-02-23 17:08:22 +0700

    update

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -10,7 +10,7 @@ class SignUpPage extends StatelessWidget {
       return Container(
         margin: EdgeInsets.only(top: 30),
         child: Text(
-          'Join Use and Get\nyour Next Journey',
+          "Join Us and Get\nyour next Journey",
           style: blackTextStyle.copyWith(
             fontSize: 24,
             fontWeight: semiBold,
@@ -20,6 +20,153 @@ class SignUpPage extends StatelessWidget {
     }
 
     Widget inputSection() {
+      Widget nameInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Username"),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBalckColor,
+                decoration: InputDecoration(
+                  hintText: "input your username",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Email"),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBalckColor,
+                decoration: InputDecoration(
+                  hintText: "input your email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget passwordInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Password"),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                obscureText: true,
+                cursorColor: kBalckColor,
+                decoration: InputDecoration(
+                  hintText: "input your password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget HobbyInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Hoby"),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBalckColor,
+                decoration: InputDecoration(
+                  hintText: "input your hoby",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      Widget submitButton() {
+        return Container(
+          width: double.infinity,
+          height: 55,
+          margin: EdgeInsets.only(bottom: 30),
+          child: TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/bonus');
+            },
+            child: Text(
+              "Submit",
+              style: whitekTextStyle.copyWith(
+                fontSize: 18,
+                fontWeight: medium,
+              ),
+            ),
+            style: TextButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(17),
+              ),
+            ),
+          ),
+        );
+      }
+
       return Container(
         margin: EdgeInsets.only(top: 30),
         padding: EdgeInsets.symmetric(
@@ -30,19 +177,46 @@ class SignUpPage extends StatelessWidget {
           color: kWitheColor,
           borderRadius: BorderRadius.circular(defaultRadius),
         ),
+        child: Column(
+          children: [
+            nameInput(),
+            emailInput(),
+            passwordInput(),
+            HobbyInput(),
+            submitButton(),
+          ],
+        ),
+      );
+    }
+
+    Widget tacButton() {
+      return Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.only(
+          top: 50,
+          bottom: 73,
+        ),
+        child: Text(
+          "Term and Condition",
+          style: greyTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: light,
+              decoration: TextDecoration.underline),
+        ),
       );
     }
 
     return Scaffold(
-      backgroundColor: kBackgroundColor,
-      body: SafeArea(
-        child: ListView(
+        backgroundColor: kBackgroundColor,
+        body: SafeArea(
+          child: ListView(
             padding: EdgeInsets.symmetric(horizontal: defaultMargin),
             children: [
               title(),
               inputSection(),
-            ]),
-      ),
-    );
+              tacButton(),
+            ],
+          ),
+        ));
   }
 }
