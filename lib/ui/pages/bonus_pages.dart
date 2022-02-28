a4eb7a91d4a8465f86f7873c0bec5ce1460cb1f9
helloaltop <helloaltop@gmail.com>
2022-03-01 02:18:20 +0700

    update

--- lib/ui/pages/bonus_pages.dart
+++ lib/ui/pages/bonus_pages.dart
@@ -6,6 +6,159 @@ class BonusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Widget bonusCard() {
+      return Container(
+        width: 300,
+        height: 211,
+        padding: EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image_card.png'),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: kPrimaryColor.withOpacity(0.5),
+              blurRadius: 50,
+              offset: Offset(0, 10),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Name',
+                        style: whitekTextStyle.copyWith(
+                          fontWeight: light,
+                        ),
+                      ),
+                      Text(
+                        'Kezia Anne',
+                        style: whitekTextStyle.copyWith(
+                          fontSize: 20,
+                          fontWeight: medium,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 24,
+                  height: 24,
+                  margin: EdgeInsets.only(right: 6),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icon_plane.png'),
+                    ),
+                  ),
+                ),
+                Text(
+                  'Pay',
+                  style: whitekTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Balance',
+                    style: whitekTextStyle.copyWith(
+                      fontWeight: light,
+                    ),
+                  ),
+                  Text(
+                    'IDR 280.000.000',
+                    style: whitekTextStyle.copyWith(
+                      fontSize: 26,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget titile() {
+      return Container(
+        margin: EdgeInsets.only(top: 80),
+        child: Text(
+          'Big Bonus',
+          style: blackTextStyle.copyWith(
+            fontSize: 32,
+            fontWeight: semiBold,
+          ),
+        ),
+      );
+    }
+
+    Widget subtitile() {
+      return Container(
+        margin: EdgeInsets.only(top: 10),
+        child: Text(
+          'We give you early credit so that\nyou can buy a flight ticket',
+          style: greyTextStyle.copyWith(
+            fontSize: 16,
+            fontWeight: light,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      );
+    }
+
+    Widget startButton() {
+      return Container(
+        width: 220,
+        height: 55,
+        margin: EdgeInsets.only(top: 50),
+        child: TextButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/main');
+          },
+          style: TextButton.styleFrom(
+            backgroundColor: kPrimaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(defaultRadius),
+            ),
+          ),
+          child: Text(
+            'Start Fly Now',
+            style: whitekTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            bonusCard(),
+            titile(),
+            subtitile(),
+            startButton(),
+          ],
+        ),
+      ),
+    );
   }
 }
